359236049d28882e31f0aba69932fc4619a384ec
solhee-hwang <solhee0912@kookmin.ac.kr>
2023-02-13 14:48:21 +0900

    Initial commit

---
+++ src/main/java/com/koss/photocarpet/PhotocarpetApplication.java
@@ -0,0 +1,13 @@
+package com.koss.photocarpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotocarpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotocarpetApplication.class, args);
+	}
+
+}
